a30ec8dc774468dfb05acec3f7e669c18851e495
Roel Meeuws <roel.meeuws@upsilon.lu>
2024-08-14 16:02:07 +0200

    add demo of issue
    Signed-off-by: Roel Meeuws <roel.meeuws@upsilon.lu>

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,23 +1,33 @@
 package com.example.application;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
+import org.apache.tomcat.util.buf.EncodedSolidusHandling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.context.annotation.Bean;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 
 /**
  * The entry point of the Spring Boot application.
- *
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
- *
  */
 @SpringBootApplication
 @Theme(value = "my-app")
-public class Application implements AppShellConfigurator {
+public class Application implements AppShellConfigurator
+{
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args)
+    {
+        System.setProperty( "org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true" );
+        SpringApplication.run( Application.class, args );
     }
 
+    @Bean
+    TomcatConnectorCustomizer connectorCustomizer()
+    {
+        return (connector) -> connector.setEncodedSolidusHandling( EncodedSolidusHandling.DECODE.getValue( ) );
+    }
 }
--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -1,10 +1,16 @@
 package com.example.application.views.helloworld;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -14,23 +20,37 @@ import com.vaadin.flow.router.RouteAlias;
 @Menu(icon = "line-awesome/svg/globe-solid.svg", order = 0)
 @Route(value = "")
 @RouteAlias(value = "")
-public class HelloWorldView extends HorizontalLayout {
+public class HelloWorldView extends HorizontalLayout implements HasUrlParameter<String>
+{
+    private static final long serialVersionUID = -3395224389731979640L;
 
-    private TextField name;
-    private Button sayHello;
+    private TextField input;
+    private Button send;
+    private TextField output;
 
-    public HelloWorldView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
+    public HelloWorldView()
+    {
+        input = new TextField( "URI parameter" );
+        send = new Button( "Send" );
+        send.addClickListener( e -> {
+            String value = input.getValue( );
+            value = URLEncoder.encode( value, StandardCharsets.UTF_8 );
+            UI.getCurrent( ).navigate( "/" + value );
+        } );
+        send.addClickShortcut( Key.ENTER );
+        output = new TextField( "Received via url" );
+        output.setReadOnly( true );
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        setMargin( true );
+        setVerticalComponentAlignment( Alignment.END, input, send, output );
 
-        add(name, sayHello);
+        add( input, send, output );
+    }
+
+    @Override
+    public void setParameter(BeforeEvent event, String parameter)
+    {
+        output.setValue( URLDecoder.decode( parameter, StandardCharsets.UTF_8 ) );
     }
 
 }
